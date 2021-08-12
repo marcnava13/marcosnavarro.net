483caf80b4397afdc41fc31a059e93ce642377a2
Marcos Navarro <marcosnpcogollos@gmail.com>
2021-08-12 18:14:54 +0200

    test: add testEnvironment options in tests setting

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
